7c8963c41c23c8b14a6eac45ac3e3342bca2a14c
Frank <frankz.lu@gmail.com>
2017-07-24 12:21:51 +0200

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: [ './app.component.css' ]
+} )
 export class AppComponent {
   title = 'app';
+  isFavourite = false;
+
+
+  courseGoals = [
+    { title: 'Master Angular Styling', isActiveGoal: true },
+    { title: 'Understand Angular Styling', isActiveGoal: false },
+    { title: 'Master Angular Animations', isActiveGoal: false },
+  ]
+
+  constructor( private renderer: Renderer2 ) {
+  }
+
+
+  onShowBoring( element: HTMLElement ) {
+    this.renderer.setStyle( element, 'display', 'block');
+  }
+
 }
